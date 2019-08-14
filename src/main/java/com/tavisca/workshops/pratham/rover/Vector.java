fc287d0ea6e4bede894c45406a7e2b74f0fe4473
Khan <mdkhan@internal.tavisca.com>
2019-08-14 22:19:11 +0530

    Downgraded the java version to Java 8

--- src/main/java/com/tavisca/workshops/pratham/rover/Vector.java
+++ src/main/java/com/tavisca/workshops/pratham/rover/Vector.java
@@ -6,29 +6,36 @@ public class Vector {
         public final int x;
         public final int y;
         public final char direction;
+        private static HashMap<Character, Character> rightMap
+            = new HashMap<>();
+
+    private static HashMap<Character, Character> leftMap
+            = new HashMap<>();
 
         public Vector(int x, int y, char direction) {
             this.x = x;
             this.y = y;
 
             this.direction = direction;
+
+            initalizeLeftMap();
+            initalizeRightMap();
         }
 
-    private static HashMap<Character, Character> leftMap
-            = new HashMap<>() {{
-        put('N', 'W');
-        put('W', 'S');
-        put('S', 'E');
-        put('E', 'N');
-    }};
 
-    private static HashMap<Character, Character> rightMap
-            = new HashMap<>() {{
-        put('N', 'E');
-        put('W', 'N');
-        put('S', 'W');
-        put('E', 'S');
-    }};
+    private void initalizeRightMap() {
+        rightMap.put('N', 'E');
+        rightMap.put('W', 'N');
+        rightMap.put('S', 'W');
+        rightMap.put('E', 'S');
+    }
+
+    private void initalizeLeftMap() {
+        leftMap.put('N', 'W');
+        leftMap.put('W', 'S');
+        leftMap.put('S', 'E');
+        leftMap.put('E', 'N');
+    }
 
     public Vector turnLeft() {
         return new Vector(this.x, this.y, leftMap.get(this.direction));

312106b3cc605077cd900497988ce7030a639b7b
Khan <mdkhan@internal.tavisca.com>
2019-08-18 22:44:31 +0530

    Changed input typr for rover problem

--- src/main/java/com/tavisca/workshops/pratham/rover/Main.java
+++ src/main/java/com/tavisca/workshops/pratham/rover/Main.java
@@ -1,9 +1,19 @@
 package com.tavisca.workshops.pratham.rover;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Rover rover = new Rover(3, 3, 'E');
-        String movements = "MMRMMRMRRM";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the initial position and direction respectively: ");
+        String position = scanner.nextLine();
+
+        Rover rover = new Rover(Integer.parseInt(position.split(" ")[0]),
+                                Integer.parseInt(position.split(" ")[1]),
+                                position.charAt(4));
+
+        System.out.println("Enter the sequence of movements: ");
+        String movements = scanner.nextLine();
 
         for (char action : movements.toCharArray()) {
             rover = rover.rove(action);
